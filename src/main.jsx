8ea27e605891c860050fb49599351e63674f218d
sgonzalez-2022140 <sgonzalez-2022140@kinal.edu.gt>
2024-05-10 23:13:45 -0600

    arreglo de ruta

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/github-blog">
+    <BrowserRouter basename="/blogPage">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
